975b2e024d6929a01ea5a14e5a4b95baeb3e2bf3
Gustavo Liu <gustavo_santostr@hotmail.com>
2019-05-14 12:52:15 -0300

    criando component novo, incompleto, falta o rodape, minuto 15:48 da aula

---
+++ src/app/rodape/rodape.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+selector: 'rodape-component'
+,templateUrl: 'rodape.component.html'
+,styleUrls: ['rodape.component.css']
+
+})
+export class RodapeComponent{
+
+}
