8ccc290ce55fc16a3e6d9bc95eaf2c5df62549cf
Gustavo Liu <gustavo_santostr@hotmail.com>
2019-05-16 12:49:20 -0300

    aula 06, minuto 11:28

--- src/app/soma/soma.component.ts
+++ src/app/soma/soma.component.ts
@@ -10,10 +10,32 @@ import { Component } from '@angular/core';
 export class SomaComponent{
   numero1 : number = 0;
   numero2 : number = 0;
+  numero3 : number = 0;
+  numero4 : number = 0;
 
   somar(): void{
     let total = Number(this.numero1) + Number(this.numero2);
     alert("A soma dos valores é: " + total);
   }
 
-}
+
+  par(): void{
+    let resultado = '';
+    if (Number(this.numero3) % 2==0) {
+      resultado = 'par';}
+      else {
+        resultado = 'impar';
+      }
+      alert(resultado);
+
+    }
+
+
+    positivo(): void{
+      let resultado: string : 'positivo';
+
+
+
+
+    }
+  }
